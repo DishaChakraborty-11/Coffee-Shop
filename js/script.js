327d89dadb06849aa72e1eaa2ab45484db8fb4ff
Disha Chakraborty <chakrabortydisha809@gmail.com>
2026-02-01 20:59:00 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -7,3 +7,21 @@ document.addEventListener("DOMContentLoaded", () => {
     })
     .catch(err => console.error(err));
 });
+
+function placeOrder() {
+  fetch("http://127.0.0.1:5000/order", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify({
+      name: "Disha",
+      item: "Cappuccino",
+      quantity: 2,
+      total: 300
+    })
+  })
+  .then(res => res.json())
+  .then(data => alert(data.message))
+  .catch(err => console.error(err));
+}
