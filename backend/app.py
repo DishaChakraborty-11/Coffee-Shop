a0d3010754b96f121dbe54b68af2fc1d83f9572a
Disha Chakraborty <chakrabortydisha809@gmail.com>
2026-02-01 20:46:37 +0530

    Create app.py

---
+++ backend/app.py
@@ -0,0 +1,21 @@
+from flask import Flask
+from flask_cors import CORS
+from config import Config
+from models import db
+from routes import api
+
+app = Flask(__name__)
+app.config.from_object(Config)
+
+CORS(app)
+db.init_app(app)
+app.register_blueprint(api)
+
+@app.route("/")
+def home():
+    return {"status": "Coffee Shop Backend Running ☕"}
+
+if __name__ == "__main__":
+    with app.app_context():
+        db.create_all()
+    app.run(debug=True)
